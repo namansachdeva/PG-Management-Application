98ee831d439072501ae5e1cb941cbda6d79b72a6
namansachdeva <namansachdevaa@gmail.com>
2017-08-12 22:14:49 +0530

    Basic Layout Design Updated + Intents added

--- app/src/main/java/com/naman/dev/royalgroup/AdminActivity.java
+++ app/src/main/java/com/naman/dev/royalgroup/AdminActivity.java
@@ -1,13 +1,47 @@
 package com.naman.dev.royalgroup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.TextView;
 
 public class AdminActivity extends AppCompatActivity {
+
+    private CardView pge38card;
+    private CardView pge114card;
+    private CardView pge1124card;
+    private CardView pgbansalcard;
+
+    private TextView impContactsbtn;
+    private TextView newGuestbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        pge38card = (CardView) findViewById(R.id.pge38_card);
+        pge114card = (CardView) findViewById(R.id.pge114_card);
+        pge1124card = (CardView) findViewById(R.id.pge1124_card);
+        pge38card =(CardView) findViewById(R.id.pge38_card);
+
+        impContactsbtn = (TextView) findViewById(R.id.imp_contacts_btn);
+        newGuestbtn = (TextView) findViewById(R.id.new_guest_btn);
+
+        impContactsbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminActivity.this,ImpContactsActivity.class));
+            }
+        });
+
+        newGuestbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminActivity.this,NewGuestActivity.class));
+            }
+        });
     }
 }
