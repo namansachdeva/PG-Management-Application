a7a3d8d42aa5ea69bc97bf3ccc873d4661a88c64
namansachdeva <namansachdevaa@gmail.com>
2017-08-22 22:50:11 +0530

    Detail Fragment designed

--- app/src/main/java/com/naman/dev/royalgroup/Fragments/DetailFragment.java
+++ app/src/main/java/com/naman/dev/royalgroup/Fragments/DetailFragment.java
@@ -1,14 +1,20 @@
 package com.naman.dev.royalgroup.Fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.naman.dev.royalgroup.R;
 
 public class DetailFragment extends Fragment {
+
+    private DatabaseReference mDatabase;
+
     public DetailFragment() {
         // Required empty public constructor
     }
@@ -22,8 +28,16 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.keepSynced(true);
+        return rootView;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
+    }
 }
