25a2f2c436694ea2286609e2876b72be708046de
namansachdeva <namansachdevaa@gmail.com>
2017-08-25 12:31:55 +0530

    Functions initialisation

--- app/src/main/java/com/naman/dev/royalgroup/Fragments/FunctionsFragment.java
+++ app/src/main/java/com/naman/dev/royalgroup/Fragments/FunctionsFragment.java
@@ -6,7 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.naman.dev.royalgroup.GuestDetailActivity;
 import com.naman.dev.royalgroup.R;
 
 /**
@@ -14,6 +18,15 @@ import com.naman.dev.royalgroup.R;
  */
 
 public class FunctionsFragment extends Fragment {
+    private DatabaseReference mDatabase;
+    private String guestKey;
+
+    private Button uploadPhotoBtn;
+    private Button uploadIDBtn;
+    private Button newPaymentBtn;
+    private Button changeRoomBtn;
+    private Button leavePGButton;
+
     public FunctionsFragment() {
         // Required empty public constructor
     }
@@ -27,6 +40,23 @@ public class FunctionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        uploadIDBtn = (Button) rootView.findViewById(R.id.uploadIDbtn);
+        uploadPhotoBtn = (Button) rootView.findViewById(R.id.uploadPhotobtn);
+        newPaymentBtn = (Button) rootView.findViewById(R.id.newPaymentbtn);
+        changeRoomBtn = (Button) rootView.findViewById(R.id.changeRoombtn);
+        leavePGButton = (Button) rootView.findViewById(R.id.leavePGbtn);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.keepSynced(true);
+
+        GuestDetailActivity guestDetailActivity = (GuestDetailActivity) getActivity();
+        guestKey = guestDetailActivity.getGuestKey();
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
     }
 }
